3cedd4bfaf0dcc7f89a5cd1240b346a505c9b954
arey0pushpa <leadnewindia@hotmail.com>
2016-02-08 19:29:12 +0530

    from Ubunut machine

--- 16thousanIterations.c
+++ 16thousanIterations.c
@@ -7,6 +7,7 @@ int main() {
    unsigned int x,y,z;
    for ( x  = 0; x < 8000; x ++) {
        for ( z =0; z < 8000; z++) {
+           x = x + 1;
           if ( 10 > 2) {
              y = 2 + 2;
           }
